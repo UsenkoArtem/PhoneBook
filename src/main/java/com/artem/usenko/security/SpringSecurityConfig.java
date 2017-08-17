6995eb1c1921a856522cd3a9c705b45f3743d728
Artem Usenko <usenko.job@gmail.com>
2017-08-17 06:21:44 +0300

    Update security

--- src/main/java/com/artem/usenko/security/SpringSecurityConfig.java
+++ src/main/java/com/artem/usenko/security/SpringSecurityConfig.java
@@ -2,7 +2,6 @@ package com.artem.usenko.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,7 +27,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic().authenticationEntryPoint(authEntryPoint)
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .antMatchers( "/api/registry/**").permitAll()
                 .antMatchers( "/login").permitAll()
                 .antMatchers( "/user").permitAll()
